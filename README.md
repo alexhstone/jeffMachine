7e6b3b50c6a182d1ed46f1288b3f2008666de4ab
jeffsDad <57819903+jeffsDad@users.noreply.github.com>
2023-01-10 17:37:48 -0600

    added scan functionality

---
+++ README.md
@@ -0,0 +1,22 @@
+Hi!  
+
+Welcome to the jeffmachine!
+
+The jeffmachine is a commandline text augmentation and analysis tool that can revolutionize the way you append .txt documents. 
+
+You can use the jeffmachine to add "Jeff" to the end of any .txt file.  
+jeffmachine will duplicate your input .txt file and make you a new one for your new jeff-i-fied message.   
+
+You can also scan a provided text file for instances of "jeff" (case insensitve!) and the jeffmachine will let you know just how many there are! 
+
+usage:  
+
+to append your txt file with a little bit of jeff, use: ./jeffmachine yourFile.txt
+to scan your provided text file for instances of jeff, use: ./jeffmachine yourFile.txt s
+
+
+
+I included a sample file, sample.txt for you to try it out on.  Simply run ./jeffmachine sample.txt to see something really cool. 
+
+the jeffmachine was created Jan 8, 2023 
+and last updated Jan 10, 2023
--- jeffmachine.c
+++ jeffmachine.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+int jeffScan (FILE *input);
 
 int main (int argc, char* argv[])
 {
     if (argc < 2)
     {
-        printf("Usage: ./jeffmachine yourFile.txt\n");
+        printf("Usage: To add a jeff to your file, use: ./jeffmachine yourFile.txt\n Or to run a jeff scan, use: ./jeffmachine yourFile.txt s\n");
         return 1;
     }
 
@@ -21,6 +25,26 @@ int main (int argc, char* argv[])
 //our design is very human, we let the user know the program is doing it's job...
 printf("Jeff machine successfully opened %s \n", argv[1]);
 
+
+//check if the user has provided additonal commandline arguments...
+if (argc > 2){
+
+    //if our user has included the "s" argument, we will run a jeff scan and printf the results to the user... 
+    if (strcmp(argv[2], "s") == 0)
+    {
+        //let the user know the scan is taking place... (and scan) 
+        printf("Your commandline arguments indidcate you wish to perform a Jeff Scan... Scanning...\n");
+        int jeffCount = jeffScan(input); 
+        printf("Scanning...\n");
+
+        //print the results to the user...
+        printf("Scan complete!  Your input file contains %i instances of 'Jeff'\n", jeffCount);
+        fclose(input);
+        return 0;
+    }
+}
+
+printf("did not activate the s argument conditional...\n");
 //we create a filename for our output format...
 char *fileName = malloc(sizeof(argv[1]) + (sizeof(char) * 5));
 sprintf(fileName, "jeff_%s", argv[1]);
@@ -53,4 +77,58 @@ free(fileName);
 fclose(output); 
 fclose(input);
 return 0;
+}
+
+
+int jeffScan(FILE *input)
+{
+
+
+    //our buffer "letter" where we can store fread value of the while that looks at each char... 
+    char *j = malloc(sizeof(char));
+
+    //our buffer array to write to (room to store jeff + nul char)
+    char *buffer = malloc(sizeof(char) * 5);
+
+    //our count variable, where we will store the number of "jeff"'s we find... 
+    int count = 0; 
+
+
+// we're looking for j's... 
+while(fread(j, sizeof(char), 1, input) == 1)
+    {
+      //we create this 'letter' char to dump the value stored at pointer j into (and we convert that to uppercase if lowercase)...
+      //this step is technically unnecessary but i think it makes the code read just a little better.
+      char letter = toupper(*j); 
+
+      //if we find a "j", we're going to see what the next 3 characters are and add them to our array...
+      if (letter == 'J')
+      {
+        //fill our array starting at the second letter... 
+        fread(buffer + 1, sizeof(char), 4, input); 
+
+
+        //...since we hit the if condintional for our j, we know the first letter is j
+        //but to avoid using a "magic letter", we'll assign the value of the letter variable from above...
+        buffer[0] = letter;
+            
+        //set our text to all upper case...
+        for (int i = 0; i < 4; i++)
+        {
+            buffer[i] = toupper(buffer[i]);
+        }
+        
+        //we also manually set a nul character here to mark the end of the string, so our conditional later doesn't get confused...
+        buffer[4] = '\0';            
+                  
+        //if our buffer values (case insensitive) spell out "JEFF, we add one to the counter... 
+        if (strcmp(buffer, "JEFF") == 0)
+            {
+            count++;
+            }
+    }
+    }
+
+    free(buffer);
+    return count;
 }
