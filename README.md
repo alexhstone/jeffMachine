79fb10aa5ee44fbcd7cb3c0b132f5070e8f85a77
jeffsDad <57819903+jeffsDad@users.noreply.github.com>
2023-01-11 17:42:51 -0600

    Added jpg functionality

--- README.md
+++ README.md
@@ -2,17 +2,20 @@ Hi!
 
 Welcome to the jeffmachine!
 
-The jeffmachine is a commandline text augmentation and analysis tool that can revolutionize the way you append .txt documents. 
+The jeffmachine is a commandline text augmentation and analysis tool that can revolutionize the way you work with .txt and .jpg files. 
 
 You can use the jeffmachine to add "Jeff" to the end of any .txt file.  
 jeffmachine will duplicate your input .txt file and make you a new one for your new jeff-i-fied message.   
 
 You can also scan a provided text file for instances of "jeff" (case insensitve!) and the jeffmachine will let you know just how many there are! 
 
+You can run jeffmachine on a .jpg file to turn it into a "jeffpg" (return an image of jeff)
+
 usage:  
 
 to append your txt file with a little bit of jeff, use: ./jeffmachine yourFile.txt
 to scan your provided text file for instances of jeff, use: ./jeffmachine yourFile.txt s
+to change your jpg (image) into an image of jeff, use: ./jeffmachine yourFile.jpg
 
 
 
--- jeffmachine.c
+++ jeffmachine.c
@@ -4,14 +4,21 @@
 #include <ctype.h>
 
 int jeffScan (FILE *input);
+char * findFileType (char * fileName);
 
 int main (int argc, char* argv[])
 {
     if (argc < 2)
     {
-        printf("Usage: To add a jeff to your file, use: ./jeffmachine yourFile.txt\n Or to run a jeff scan, use: ./jeffmachine yourFile.txt s\n");
+        printf("Usage: To add a jeff to a text file, use: ./jeffmachine yourFile.txt\n Or to run a jeff scan, use: ./jeffmachine yourFile.txt s\n");
+        printf("The Jeff Machine can now accept 'jpg' files as well!  Usage: ./jeffmachine yourFile.jpg");
         return 1;
     }
+    
+    typedef uint8_t BYTE;
+    
+    //figure out the file type of our input file, so we know what to do with it.    
+    char * fileType = findFileType(argv[1]);
 
     //open our input file...
     FILE *input = fopen(argv[1], "r");
@@ -44,20 +51,20 @@ if (argc > 2){
     }
 }
 
-printf("did not activate the s argument conditional...\n");
+
 //we create a filename for our output format...
 char *fileName = malloc(sizeof(argv[1]) + (sizeof(char) * 5));
 sprintf(fileName, "jeff_%s", argv[1]);
 
-//and we tell the user that too, so they don't get anxious. 
 printf("Creating file %s \n", fileName);
-
-//since we're not a liar, we do, infact, create that file... 
 FILE *output = fopen(fileName, "w");
 
+
+//if our file is a text file...
+if (strcmp(fileType, "txt") == 0)
+{
 //create our buffer array...
 char buffer[sizeof(char) * 2];
-
 
 //now we read the file in chunks of 2 (arbitrarily) and print those chunks to our output file...
 while (fread(buffer, sizeof(char), 2, input) == 2)
@@ -71,8 +78,33 @@ fprintf(output, " Jeff\n");
 
 //let the user know that the program has successfully run!
 printf("Congratulations! Your input file has been Jeff-i-fied! Woah! Check out the results in %s\n", fileName);
+}
+
+if (strcmp(fileType, "jpg") == 0)
+{
+   //open our jeff pic... 
+   FILE *jeffPic = fopen("./jeff.jpg", "r");
+   if (jeffPic == NULL)
+   {
+        printf("Something has gone wrong... Jeff image not found... Please contact support \n");
+        return 2;
+   }
+
+   //buffer for our read/write below...
+   BYTE buffer[512];
+
+   //copy jeff pic to outputfil 
+   while(fread(buffer, sizeof(BYTE), 512, jeffPic) == 512)
+   {
+    fwrite(buffer, sizeof(BYTE), 512, output);
+   }
+
+    printf("Success!  Your image has been Jeff-i-fied! Open up %s to see your new image... \n", fileName);
+    fclose(jeffPic);
+}
 
 //and clean up our mess... 
+free(fileType);
 free(fileName);
 fclose(output); 
 fclose(input);
@@ -82,8 +114,6 @@ return 0;
 
 int jeffScan(FILE *input)
 {
-
-
     //our buffer "letter" where we can store fread value of the while that looks at each char... 
     char *j = malloc(sizeof(char));
 
@@ -131,4 +161,21 @@ while(fread(j, sizeof(char), 1, input) == 1)
 
     free(buffer);
     return count;
+}
+
+char * findFileType(char* fileName)
+{
+    //find the length of the input string, subtract 1, and assign it to int 'last char'
+    int lastchar = strlen(fileName) - 1;
+
+    //allocate memory for our 3 digit filetype string...
+    char* fileType = malloc(sizeof(char) * 4); 
+
+    //add the last three digits of the input string to our 
+    sprintf(fileType, "%c%c%c", fileName[lastchar - 2], fileName[lastchar - 1], fileName[lastchar]);
+
+    //assign a NUL to denote the end of our string.. 
+    fileType[3] = '\0'; 
+
+    return fileType;
 }
